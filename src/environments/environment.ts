39fcc6180486d9804c4d15724e2be43bca59452d
Luciano Murruni <luciano.murruni@gmail.com>
2023-03-24 21:51:32 +0100

    add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'ng-fire-fa487',
+    appId: '1:810751441970:web:612586f49dab1f7c45502b',
+    databaseURL: 'https://ng-fire-fa487-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'ng-fire-fa487.appspot.com',
+    apiKey: 'AIzaSyAtKxa3a4ZE9teIfoOAXJ7xuKlahuHuLx8',
+    authDomain: 'ng-fire-fa487.firebaseapp.com',
+    messagingSenderId: '810751441970',
+    measurementId: 'G-ZTB5CQ4RXB',
+  },};
